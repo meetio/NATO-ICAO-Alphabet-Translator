7cbfa5bb9b6e77c65efacd77430553ff470a4d62
Johan Nordstrom <johan@digitalvillage.se>
2016-08-30 10:57:02 +0200

    Rewritten, simplified and a small bit improved :).

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-NATO Alphabet Translator
+PHP NATO Alphabet Translator
 =======================
 
-This is a PHP class that translates phrases into the NATO(ICAO) Phonetic Alphabet.
+This is a PHP class that translates phrases into the NATO(ICAO) Phonetic Alphabet. Fork of [justindmartin/NATO-ICAO-Alphabet-Translator](https://github.com/justindmartin/NATO-ICAO-Alphabet-Translator) but rewritten, simplified and a small bit improved :).
 
 ## Usage Example
 
 ```PHP
-//initialize NATOAlphabetTranslator class
-$NATOAlphabetTranslator = new NATOAlphabetTranslator();
+// initialize NATOAlphabetTranslator class
+require('src/NATOAlphabetTranslator.php');
+$mNATOAlphabetTranslator = new NATOAlphabetTranslator();
 
-//translate a phrase using the NATO/ICAO alphabet
-$NATOAlphabetTranslator->translateToICAO('My name is Justin. I am a software developer.');
+// translate a phrase using the NATO/ICAO alphabet
+echo $mNATOAlphabetTranslator->translateToICAO("aiF9a-iqu");
+> alpha india Foxtrot nine alpha [dash] india quebec uniform
 ```
 
 <img src="http://upload.wikimedia.org/wikipedia/commons/e/e0/FAA_Phonetic_and_Morse_Chart2.svg"/>
-
-
--- src/NATOAlphabetTranslator.php
+++ src/NATOAlphabetTranslator.php
@@ -1,52 +1,55 @@
 <?php
-class NATOAlphabetTranslator{
-	function __Construct(){
-		$this->normalAlphabet = 'abcdefghijklmnopqrstuvwxyz'; //alphabet positions
-		$this->translationAlphabet = array('Alpha',
-						   'Bravo',
-						   'Charlie',
-						   'Delta',
-				        	   'Echo',
-						   'Foxtrot',
-        	                                   'Golf',
-						   'Hotel',
-						   'India',
-						   'Juliet',
-                                        	   'Kilo',
-						   'Lima',
-						   'Mike',
-						   'November',
-                        	                   'Oscar',
-                                	           'Papa',
-	                                           'Quebec',
-        	                                   'Romeo',
-                	                           'Sierra',
-                        	                   'Tango',
-                                	           'Uniform',
-                                        	   'Victor',
-	                                           'Whiskey',
-					           'X-ray',
-						   'Yankee',
-                        	                   'Zulu'
-						  ); //English to NATO Phonetic translation alphabet
-	}
-	function translateToICAO($phraseToTranslate){
-		$phraseTranslated = strtolower($phraseToTranslate); //convert to lowercase
-                $phraseTranslated = preg_replace('/[^a-z0-9. ]+/i', '', $phraseTranslated); //remove all non-letters,non-numbers and puncutation except spaces and periods
-		$phraseTranslated = preg_replace("/(.)/i","\${1} ",$phraseTranslated); //add space between each word
-		$phraseLength = strlen($phraseTranslated); //get initial phrase length
-		for($i = 0; $i < $phraseLength; $i++){
-			$currentCharacter = substr($phraseTranslated,$i,1); //get character to replace
-			$translationIndex = strpos($this->normalAlphabet,$currentCharacter); //find current character's translation index
-			if($translationIndex !== false){
-				$phraseTranslated = substr_replace($phraseTranslated,$this->translationAlphabet[$translationIndex],$i,1); //replace character with translation
-				$i += (strlen($this->translationAlphabet[$translationIndex])-1); //update counter(i) with new position according to change string length
-				$phraseLength = strlen($phraseTranslated); //update phrase length
-			}
+
+class NATOAlphabetTranslator {
+
+	private $translationAlphabet = array(
+		'a' => 'Alpha',
+		'b' => 'Bravo',
+		'c' => 'Charlie',
+		'd' => 'Delta',
+		'e' => 'Echo',
+		'f' => 'Foxtrot',
+		'g' => 'Golf',
+		'h' => 'Hotel',
+		'i' => 'India',
+		'j' => 'Juliet',
+		'k' => 'Kilo',
+		'l' => 'Lima',
+		'm' => 'Mike',
+		'n' => 'November',
+		'o' => 'Oscar',
+		'p' => 'Papa',
+		'q' => 'Quebec',
+		'r' => 'Romeo',
+		's' => 'Sierra',
+		't' => 'Tango',
+		'u' => 'Uniform',
+		'v' => 'Victor',
+		'w' => 'Whiskey',
+		'x' => 'X-ray',
+		'y' => 'Yankee',
+		'z' => 'Zulu',
+		'0' => 'Zero',
+		'1' => 'One',
+		'2' => 'Two',
+		'3' => 'Three',
+		'4' => 'Four',
+		'5' => 'Five',
+		'6' => 'Six',
+		'7' => 'Seven',
+		'8' => 'Eight',
+		'9' => 'Nine',
+		'-' => '[dash]',
+		'.' => '[dot]',
+		' ' => '[space]'
+	); //English to NATO Phonetic translation alphabet
+
+	function translateToICAO($str) {
+		$nato = [];
+		foreach (str_split($str) as $char) {
+			$tr = isset($this->translationAlphabet[strtolower($char)]) ? $this->translationAlphabet[strtolower($char)] : $char;
+			array_push($nato,  ctype_upper($char) ? $tr : strtolower($tr));
 		}
-		$phraseTranslated = str_replace(' .','.',$phraseTranslated); //remove extra space preceding periods
-		$phraseTranslated = str_replace(array(0,1,2,3,4,5,6,7,8,9),array('Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'),$phraseTranslated); //translate numbers
-		return $phraseTranslated; // return translated phrase
+		return implode(" ", $nato);
 	}
 }
-?>
